4f26ffb4e76648b516da5f30b38277a3c27d4b3f
nykim12 <nykim9001@gmail.com>
2022-04-18 15:24:29 +0900

    20220418

--- MVC/src/main/java/controller/MyController.java
+++ MVC/src/main/java/controller/MyController.java
@@ -13,34 +13,36 @@ import model.Myservice;
 import model.Now;
 import model.Today;
 
-//	suffix값이 .do 인 모든 URLMapping 을 처리하라
+// suffix값이 .do인 모든 URLMapping을 처리하시오.
 @WebServlet("*.do")
+
 public class MyController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public MyController() {
 		super();
-
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-//		요청
+		// 요청
 		request.setCharacterEncoding("UTF-8");
 
-//		/MVC/today.to	/MVC/now.do	구분 방법
-		String requestURI = request.getRequestURI(); // /MVC/today.to
+		// /MVC/today.do /MVC/now.do 구분하는 방법
+		String requestURI = request.getRequestURI(); // /MVC/today.do
 		String contextPath = request.getContextPath(); // /MVC
-		String command = requestURI.substring(contextPath.length() + 1); // /today.to
+		String command = requestURI.substring(contextPath.length() + 1); // today.do
 
-//		모든 model은 MyService 인터페이스를 구현하는 클래스이므로, MyService 타입의 인스턴스이다.
+		// 모든 model은 MyService 인터페이스를 구현하는 클래스이므로,
+		// MyService 타입의 인스턴스이다.
 		Myservice service = null;
 
-//		ActionForward 인스턴스
+		// ActionForward 인스턴스
 		ActionForward af = null;
 
 		switch (command) {
+		// model이 사용되는 경우
 		case "today.do":
 			service = new Today();
 			break;
@@ -50,18 +52,21 @@ public class MyController extends HttpServlet {
 		case "lotto.do":
 			service = new Lotto();
 			break;
+		// model이 사용되지 않는 단순 이동
+		case "input.do":
+			af = new ActionForward();
+			af.setRedirect(false);
+			af.setView("views/input.jsp");
+			break;
 		}
 
-//		model의 실행(execute() 메소드의 호출)
+		// model의 실행(execute() 메소드의 호출)
 		if (service != null) {
 			af = service.execute(request, response);
 		}
 
-//		model이 반환한 어디로 어떻게 정보(ActionForward)를 이용해서 이동
-//		af가 null인 경우가 있음
-//		 (1) model에서 직접 response를 이용해 응답한 경우
-//		 (2) ajax 처리
-
+		// model이 반환한 어디로 어떻게 정보(ActionForward)를 이용해서 이동
+		// af가 null인 경우가 있다. (모델에서 직접 response를 이용해 응답한 경우, ajax 처리)
 		if (af != null) {
 			if (af.isRedirect()) {
 				response.sendRedirect(af.getView());
@@ -70,14 +75,10 @@ public class MyController extends HttpServlet {
 			}
 		}
 
-//		request를 응답 View로 전달(forward)한다.
-		request.getRequestDispatcher("/views/output.jsp").forward(request, response);
-
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
--- MVC/src/main/webapp/index.jsp
+++ MVC/src/main/webapp/index.jsp
@@ -7,6 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-<h1>홈페이지에 오신 것을 환영합니다.</h1>
+
+	<h1>홈페이지에 오신 걸 환영합니다.</h1>
+	
+	<a href="/MVC/input.do">input.jsp로 이동하기</a>
+
 </body>
 </html>
