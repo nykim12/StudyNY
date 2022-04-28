5f0ac0251b7cf79da280c966b8110e264de28dde
GDJ45 <GDJ45@192.168.101.18>
2022-04-28 16:02:24 +0900

    20220428

--- JUNIT/src/main/java/test/ProductTestCase.java
+++ JUNIT/src/main/java/test/ProductTestCase.java
@@ -1,9 +1,7 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.List;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import dbcp.ProductDAO;
 import dbcp.ProductDTO;
