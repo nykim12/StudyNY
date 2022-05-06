1cd0fb793bc141163739d5734cc02d178810b97b
nykim12 <nykim9001@gmail.com>
2022-05-06 14:39:09 +0900

    20220506

--- spring/src/main/java/com/goodee/ex01/xml01/EngineerCalculator.java
+++ spring/src/main/java/com/goodee/ex01/xml01/EngineerCalculator.java
@@ -2,4 +2,52 @@ package com.goodee.ex01.xml01;
 
 public class EngineerCalculator {
 
+	private int a;
+	private int b;
+	private Calculator calculator;
+
+	public void add() {
+		calculator.add(a, b);
+	}
+
+	public void sub() {
+		calculator.sub(a, b);
+	}
+
+	public void mul() {
+		calculator.mul(a, b);
+	}
+
+	public void div() {
+		calculator.div(a, b);
+	}
+
+	public void mod() {
+		calculator.mod(a, b);
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public Calculator getCalculator() {
+		return calculator;
+	}
+
+	public void setCalculator(Calculator calculator) {
+		this.calculator = calculator;
+	}
+
 }
