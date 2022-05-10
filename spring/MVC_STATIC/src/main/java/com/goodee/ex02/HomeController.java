33181e0f8ab086de9cd5733444441eb6ec92e590
nykim12 <nykim9001@gmail.com>
2022-05-10 13:04:23 +0900

    20220510

---
+++ spring/MVC_STATIC/src/main/java/com/goodee/ex02/HomeController.java
@@ -0,0 +1,42 @@
+package com.goodee.ex02;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/*
+	@Controller
+	클래스로 만드는 컨트롤러
+
+*/
+@Controller
+public class HomeController {
+
+//	JSP처럼 *.do와 같이 공통 URLMapping을 가질 필요 X
+
+//	메소드 1개 = 요청 1개, 응답 1개
+
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "home";
+	}
+
+}
