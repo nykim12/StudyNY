83bfbe38fa4f24f52da5ebc382852e81b7604cd8
nykim12 <nykim9001@gmail.com>
2022-05-11 10:10:16 +0900

    20220511

--- spring/MVC_DI/src/main/java/com/goodee/ex03/controller/BoardController.java
+++ spring/MVC_DI/src/main/java/com/goodee/ex03/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.goodee.ex03.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.goodee.ex03.domain.Board;
 
 @Controller
 public class BoardController {
@@ -12,9 +15,18 @@ public class BoardController {
 //	@GetMapping
 //	@PostMapping
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String index() {
-		return "index";
+		return "index"; // 뷰 리졸버에 의해 "/WEB-INF/views/index.jsp"로 변환
+	}
+
+	private Board board1;
+
+//	GetMapping("board/detail") 또는 GetMapping("/board/detail")
+	@GetMapping("board/detail")
+	public String board1(HttpServletRequest request) {
+		request.setAttribute("board1", board1);
+		return "board/detail";
 	}
 
 }
